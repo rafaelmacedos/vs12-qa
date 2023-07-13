5dae4137156de4fb62e687b6b28a8d50a7205f57
Rafael Macedo <macedorafael2002@gmail.com>
2023-07-13 19:56:33 -0300

    Resolvido sexto exercício da task01

---
+++ JAVA/MÓDULO 1/java-poo-task01/src/ex06.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class ex06 {
+    public static void main(String[] args) {
+            // Declarando o scanner e recebendo como paramêtro a entrada do sistema
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("\n" +
+                    "  _______  __           ___   __   \n" +
+                    " | ____\\ \\/ /          / _ \\ / /_  \n" +
+                    " |  _|  \\  /   _____  | | | | '_ \\ \n" +
+                    " | |___ /  \\  |_____| | |_| | (_) |\n" +
+                    " |_____/_/\\_\\          \\___/ \\___/ \n" +
+                    "                                   \n");
+
+            System.out.println("Eleições...\n");
+
+            // Abaixo peço ao usuário que faça as entradas dos votos para fazer o calculo de proporção
+            System.out.print("Digite o número total de eleitores: ");
+            int eleitores = Integer.parseInt(sc.nextLine());
+
+            System.out.print("Digite o número de votos em branco: ");
+            int brancos = Integer.parseInt(sc.nextLine());
+
+            System.out.print("Digite o número de votos nulos: ");
+            int nulos = Integer.parseInt(sc.nextLine());
+
+            System.out.print("Digite o número de votos válidos: ");
+            int validos = Integer.parseInt(sc.nextLine());
+
+            // Aqui calculo a porcentagem
+            double porcentagemBrancos = (brancos * 100.0) / eleitores;
+            double porcentagemNulos = (nulos * 100.0) / eleitores;
+            double porcentagemValidos = (validos * 100.0) / eleitores;
+
+            // Apresento as porcentagens de cada tipo de voto
+            System.out.println("Porcentagem de votos em branco: " + porcentagemBrancos + "%");
+            System.out.println("Porcentagem de votos nulos: " + porcentagemNulos + "%");
+            System.out.println("Porcentagem de votos válidos: " + porcentagemValidos + "%");
+
+    }
+}
