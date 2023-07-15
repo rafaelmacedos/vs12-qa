3c1a10013a964752866f84a5d50ec209917d68b8
Rafael Macedo <macedorafael2002@gmail.com>
2023-07-15 17:15:54 -0300

    Resolvido quinto exercício da task02

--- JAVA/MÓDULO 1/java-poo/src/ExAula02for.java
+++ JAVA/MÓDULO 1/java-poo/src/ExAula02for.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class exAula02for {
+public class ExAula02for {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] inteiros = new int[5];
